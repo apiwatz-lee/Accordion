9f936431f4484999715c47c028e7d8d75ba65817
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-10-29 22:53:38 +0700

    feat:user can click icon to see details on each title

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import SingleContent from "./components/SingleContent";
+import data from "./data";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [content, setContent] = useState(data);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="flex flex-col justify-center items-center min-h-screen w-screen">
+      <h3 className="text-3xl font-bold">Frontend Developer tools 2023</h3>
+      <section className="w-[60%]">
+        {content.map((data) => {
+          return <SingleContent key={data.id} {...data} />; //The spread operator can significantly simplify your code. For example, it allows you to pass all props to a child component in a single line, rather than passing them one by one.
+        })}
+      </section>
+    </main>
   );
 }
 
